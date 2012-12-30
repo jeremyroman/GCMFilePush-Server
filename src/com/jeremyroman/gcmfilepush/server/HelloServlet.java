d4b3477855c12e3cb1095ed0342de515312e735d
Jeremy Roman <jeremy@jeremyroman.com>
2012-12-30 15:27:27 -0500

    starting with hello world

---
+++ src/com/jeremyroman/gcmfilepush/server/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.jeremyroman.gcmfilepush.server;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/plain");
+    response.getWriter().println("Hello, world!");
+  }
+}
